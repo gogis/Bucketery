19f6e1c3cb01ae9a1564f81c361d8f6c2391dd4a
ogogizm <ogogizm@gmail.com>
2017-10-11 10:29:15 +0300

    dev

---
+++ bucketery/constants.h
@@ -0,0 +1,3 @@
+#pragma once
+
+size_t const no_of_indices_on_index_page = 10000;
--- bucketery/indexer.cpp
+++ bucketery/indexer.cpp
@@ -2,6 +2,9 @@
 
 #include "indexer.h"
 
+#include "constants.h"
+
 indexer::indexer(file& file_storage) : file_(file_storage)
 {
 }
+
